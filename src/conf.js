4c27dc2538698e21cfdf2d51b35b27383a904d53
Ishaan0001i <73296159+Ishaan0001i@users.noreply.github.com>
2021-12-23 22:07:09 +0530

    Forms updated

--- src/conf.js
+++ src/conf.js
@@ -1,6 +1,6 @@
 export const MAINTENANCE_FORM_LINK =
-	"https://docs.google.com/forms/d/e/1FAIpQLSeyJnfRhIFGWbEamf-RsoB8wO6pGj_Os3s-fmwJtzKZa2enPg/viewform?embedded=true";
+	"https://docs.google.com/forms/d/e/1FAIpQLSePn2m2YguhddyLhfXx3HigUBPh-FzyS40lia7eZN4RwKEEOw/viewform?embedded=true";
 export const CONTACT_FORM_LINK =
-	"https://docs.google.com/forms/d/e/1FAIpQLSc5eJKnJZZMZzPSn6lmucog9LCU_LI-OQ7lAF9WHy_oaXad8g/viewform?embedded=true";
+	"https://docs.google.com/forms/d/e/1FAIpQLSc3iP_QQg6w0qiXG0pvLRIUPsakNBUyPoEXR7x9-iSfkntJ_g/viewform?embedded=true";
 // export const CONTACT_FORM_LINK =
 // 	"https://docs.google.com/forms/d/e/1FAIpQLSc5eJKnJZZMZzPSn6lmucog9LCU_LI-OQ7lAF9WHy_oaXad8g/viewform?embedded=true";
